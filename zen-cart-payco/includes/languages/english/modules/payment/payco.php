8f40385b327fb18d9898a44a1f356173c3d1d5bc
JuanEstebanGarciaV <juan.garcia@epayco.com>
2024-08-02 13:00:16 -0500

    Se actualiza logo de ePayco

--- zen-cart-payco/includes/languages/english/modules/payment/payco.php
+++ zen-cart-payco/includes/languages/english/modules/payment/payco.php
@@ -7,21 +7,18 @@
   Released under the GNU General Public License
 */
 
-	//declaracion de constantes las cuales van a utilizar zencart en el lenguaje ingles
-	
-	
-  //define('MODULE_PAYMENT_PAYCO_TEXT_TITLE', 'Pasarela de Pago Seguro a través de Payco.co');
-	  define('MODULE_PAYMENT_PAYCO_TEXT_TITLE', '<img src="http://secure2.payco.co/PLUGINS_IMG/logo_zen_cart.png">');
-  	define('MODULE_PAYMENT_PAYCO_TEXT_DESCRIPTION', 'PAYCO');
-  	define('MODULE_PAYMENT_PAYCO_TEXT_ERROR_MESSAGE','Error in the process');
-  	define('MODULE_PAYMENT_PAYCO_TEXT_CANCEL','The process Canceled');
-	  define('MODULE_PAYMENT_PAYCO_TEXT_CREDIT_CARD_TYPE', 'Card Type:');
-  	define('MODULE_PAYMENT_PAYCO_TEXT_CREDIT_CARD_OWNER', 'Credit Card Owner:');
-  	define('MODULE_PAYMENT_PAYCO_TEXT_CREDIT_CARD_NUMBER', 'Number Card:');
-  	define('MODULE_PAYMENT_PAYCO_TEXT_CREDIT_CARD_EXPIRES', 'Expiration Date:');
-  	define('MODULE_PAYMENT_PAYCO_TEXT_JS_PAYCO_OWNER', '* The owner\'s name of the credit card must be at least ' . CC_OWNER_MIN_LENGTH . ' letras.\n');
-  	define('MODULE_PAYMENT_PAYCO_TEXT_JS_PAYCO_NUMBER', '* The credit card number must be at least ' . CC_NUMBER_MIN_LENGTH . ' numeros.\n');
-  	define('MODULE_PAYMENT_PAYCO_TEXT_ERROR', 'Credit Card Error!');   
-	
-	     
-?>
+//declaracion de constantes las cuales van a utilizar zencart en el lenguaje ingles
+
+
+//define('MODULE_PAYMENT_PAYCO_TEXT_TITLE', 'Pasarela de Pago Seguro a travï¿½s de Payco.co');
+define('MODULE_PAYMENT_PAYCO_TEXT_TITLE', '<img src="https://plugins.epayco.io/testing/prueba/mediosdepago.png" width="290">');
+define('MODULE_PAYMENT_PAYCO_TEXT_DESCRIPTION', 'PAYCO');
+define('MODULE_PAYMENT_PAYCO_TEXT_ERROR_MESSAGE', 'Error in the process');
+define('MODULE_PAYMENT_PAYCO_TEXT_CANCEL', 'The process Canceled');
+define('MODULE_PAYMENT_PAYCO_TEXT_CREDIT_CARD_TYPE', 'Card Type:');
+define('MODULE_PAYMENT_PAYCO_TEXT_CREDIT_CARD_OWNER', 'Credit Card Owner:');
+define('MODULE_PAYMENT_PAYCO_TEXT_CREDIT_CARD_NUMBER', 'Number Card:');
+define('MODULE_PAYMENT_PAYCO_TEXT_CREDIT_CARD_EXPIRES', 'Expiration Date:');
+define('MODULE_PAYMENT_PAYCO_TEXT_JS_PAYCO_OWNER', '* The owner\'s name of the credit card must be at least ' . CC_OWNER_MIN_LENGTH . ' letras.\n');
+define('MODULE_PAYMENT_PAYCO_TEXT_JS_PAYCO_NUMBER', '* The credit card number must be at least ' . CC_NUMBER_MIN_LENGTH . ' numeros.\n');
+define('MODULE_PAYMENT_PAYCO_TEXT_ERROR', 'Credit Card Error!');
